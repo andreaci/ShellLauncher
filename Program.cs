85a9bdbeb36cf093a3bf6042cad91873f56006a3
andreaci <cortiandrea@hotmail.it>
2021-04-26 17:17:30 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShellLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                string path = @"C:\Helios\exe\";
+                string exe = "Helios_RDP01.exe";
+
+                if (args != null && args.Length == 1)
+                {
+                    FileInfo temp = new FileInfo(args[0]);
+                    path = temp.DirectoryName;
+                    exe = temp.Name;
+                }
+
+                ProcessStartInfo info = new ProcessStartInfo(Path.Combine(path, exe))
+                {
+                    WorkingDirectory = path
+                };
+
+                Process process = Process.Start(info);
+                process.WaitForExit();
+
+                Thread.Sleep(5000);
+            } while (true);
+        }
+    }
+}
